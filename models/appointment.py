736aa52ea51442e0c112bbf735f65ac52597bf59
RaihanSnh <nathahamzahraihansatya@gmail.com>
2024-01-21 21:47:47 +0700

    feat: inherit mail thread

--- models/appointment.py
+++ models/appointment.py
@@ -4,36 +4,40 @@ from datetime import timedelta
 
 class Appointment(models.Model):
     _name = 'dental.care.appointment'
+    _inherit = 'mail.thread'
     _description = 'Data Appointments'
 
     patient_id = fields.Many2one(
         string='Patient',
         comodel_name='res.partner',
         domain="[('is_patient', '=', True)]",
-        required=True
+        required=True,
+        track_visibility='onchange'
     )
 
     doctor_id = fields.Many2one(
         string='Doctor',
         comodel_name='res.partner',
         domain="[('is_doctor', '=', True)]",
-        required=True
+        required=True,
+        track_visibility='onchange'
     )
 
     service_id = fields.Many2one(
         string='Service',
         comodel_name='dental.care.service',
         ondelete='restrict',
+        track_visibility='onchange'
     )
 
     date_start = fields.Datetime(
-        'Start', required=True, tracking=True, default=fields.Date.today,
+        'Start', required=True, tracking=True, default=fields.Date.today, track_visibility='onchange',
         help="Start Date Appointment")
     
     
     date_end = fields.Datetime(
         'End', required=True, tracking=True, default=lambda self: fields.Datetime.today() + timedelta(hours=0.5),
-        compute='_compute_stop', readonly=False, store=True,
+        compute='_compute_stop', readonly=False, store=True, track_visibility='onchange',
         help="End Date Appointment")
     
     state = fields.Selection(
@@ -41,7 +45,8 @@ class Appointment(models.Model):
         selection=[('schedule', 'Schedule'), ('pending', 'Pending'), ('examination', 'Examination'), ('cancel', 'Cancel')],
         readonly=True,
         default='schedule',
-        required=True
+        required=True,
+        track_visibility='onchange'
     )
 
     def action_pending(self):
